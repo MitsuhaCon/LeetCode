9df2d9a1df6d204e2fffa9fc12e0be1e596b326b
MitsuhaCon <mitsuhaCon@gmail.com>
2020-08-15 11:17:14 +0800

    asd

---
+++ src/com/mitsuha/twoadd/M.java
@@ -0,0 +1,13 @@
+package com.mitsuha.twoadd;
+
+/**
+ * @Author yangliming
+ * @Classname M
+ * @Description //TODO
+ * @Data 2020/8/15 11:13
+ */
+public class M {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
